46ca47442e0e711800be68b79ee18d2753abb97c
Duedot43 <jackgendill@icloud.com>
2022-01-12 22:03:48 -0700

    Added a clean script

---
+++ src/clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#set this up in a crontab and set it to run after every school day to archive the logs and clean students from the departed folder
+mv -v departed/* registered_phid/
+name=$(date '+%Y-%m-%d')
+mv log/inout.log log/$name.log.old
