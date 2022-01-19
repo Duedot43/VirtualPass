e604a50f63ff90496badfd29f241438ae4b1f1f4
Duedot43 <jackgendill@icloud.com>
2022-01-19 16:09:13 -0700

    Added logging for the regestratin of qrids to the log file

--- src/regqrid.php
+++ src/regqrid.php
@@ -2,6 +2,13 @@
 $rnum=$_POST['rnum'];
 $fh = fopen('qrid.txt','r');
 $qrid = fgets($fh);
+$date = exec("date");
+echo("you have {$dpt}");
+exec("echo ///////////////////////////////////////////////// >> log/inout.log");
+exec("echo '{$date}' >> log/inout.log");
+//echo($cookid);
+exec("echo 'qrid {$qrid} registred to room {$rnum}' >> log/inout.log");
+exec("echo ///////////////////////////////////////////////// >> log/inout.log");
 exec("cd registerd_qrids/ && echo '{$rnum}' >> {$qrid}");
 header("Location: /stupid.php");
 exit()
