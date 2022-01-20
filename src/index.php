87581e85564369456b6faa4fe64a445f67ee5880
Duedot43 <jackgendill@icloud.com>
2022-01-20 12:19:18 -0700

    added invalid URL function

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
 
 <?php 
-
+if ($_GET['page'] == "") {
+    echo("invalid URL");
+    exit();
+}
 //echo "TYPE THIS NUMBER IN THE BOX >>> ", $_GET['page'];
 exec("rm qrid.txt && echo " . $_GET['page'] . " > qrid.txt");
 header("Location: /stupid.php");
