eea721da4c17ce8004218d6289a6d8e49e0f1899
Duedot43 <jackgendill@icloud.com>
2022-01-20 09:58:39 -0700

    Added cookie expiration date

--- src/registercookie.php
+++ src/registercookie.php
@@ -6,7 +6,7 @@ $stid=$_POST['stid'];
 $stem=$_POST['stem'];
 $ranid = uniqid(rand());
 echo $ranid;
-setcookie($cookie_name, $ranid);
+setcookie($cookie_name, $ranid, time() + (86400 * 360));
 exec("cd registered_phid/ && echo '{$firstname}' '{$lastname}' '{$stid}' '{$stem}' >> {$ranid}");
 exec("echo '{$firstname} registered with phid {$ranid} >> log/inout.log");
 exec("echo ///////////////////////////////////////////////// >> log/inout.log");
