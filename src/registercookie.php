9915f8873612ef9a2e7553a44966d0f90593a67b
Duedot43 <jackgendill@icloud.com>
2022-04-19 12:40:32 -0600

    Testin

--- src/registercookie.php
+++ src/registercookie.php
@@ -97,7 +97,7 @@ if(isset($_GET['page'])) {
                 copy("usr_pre_fls/index.php", "administrator/student.php");
                 //exec("cp usr_pre_fls/index.php ./administrator/student.php");
               }
-              $tat = '<link href="style.css" rel="stylesheet" type="text/css" /><input class="reg" type="button" value="' . $firstname . '" onclick="location=\'/human_info/' . $ranid . '/index.html\'" /></td>';
+              $tat = '<link href="/style.css" rel="stylesheet" type="text/css" /><input class="reg" type="button" value="' . $firstname . ' ' . $lastname . ' <?php $ini = parse_ini_file("../../registered_phid/' . $ranid . '"); echo($ini["student_activity"]);?>" onclick="location=\'/human_info/' . $ranid . '/index.html\'" /></td>';
               $student = file_put_contents('administrator/student.php', $tat.PHP_EOL , FILE_APPEND | LOCK_EX);
               if (!is_file("human_info/teacher_portal/" . $qrid . ".php")){
                 copy("usr_pre_fls/index_teacher_other.php", "human_info/teacher_portal/" . $qrid . ".php");
