516ea4bb991a967efbd6f419d638f600e9293598
Duedot43 <jackgendill@icloud.com>
2022-05-04 23:08:36 -0600

    fixed deluser

--- src/delusr.php
+++ src/delusr.php
@@ -13,9 +13,14 @@ $cookie_name = "phid";
 if(isset($_COOKIE[$cookie_name])){
     check_phid($_COOKIE[$cookie_name]);
     if (file_exists("registered_phid/" . $_COOKIE[$cookie_name])){
-        //exec("rm -rf departed/" . $_COOKIE[$cookie_name]);
+        copy("registered_phid/" . $_COOKIE[$cookie_name], "human_info/" . $_COOKIE[$cookie_name] . "/archived_ini");
         unlink("registered_phid/" . $_COOKIE[$cookie_name]);
-        //exec("rm registered_phid/" . $_COOKIE[$cookie_name]);
+        $main_json = json_decode(file_get_contents("../mass.json"), true);
+        $main_json['user'] = \array_diff($main_json['user'], [$_COOKIE[$cookie_name]]);
+        array_push($main_json['removed'], $_COOKIE[$cookie_name]);
+        $json_out = fopen("../mass.json", "w");
+        fwrite($json_out, json_encode($main_json));
+        fclose($json_out);
         setcookie("phid", "", time() - 9999999999);
     }else{
         echo("Internal server error your file is not here! please try again...");
