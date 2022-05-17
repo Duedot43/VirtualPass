5131bfcefc373e18e94f63714c769861b0fa3bfd
Duedot43 <jackgendill@icloud.com>
2022-05-17 09:22:05 -0600

    Removed a warning for teacher/menu.php

--- src/teacher/menu.php
+++ src/teacher/menu.php
@@ -51,16 +51,6 @@ if ($sendemail == "1"){
 if ($sendemail == "0"){
     $enable_email = "Enable Emails";
 }
-if ($ini['updates'] == 1){
-    //This is an option for updates the update server is in the repo vp-update not really a good feture so im just gonna leave it off by default
-$remote_release = file_get_contents("https://85c5-8-48-134-44.ngrok.io/latest");
-$remote_merge_info = "https://85c5-8-48-134-44.ngrok.io/release_folder/merge-info";
-$remote_merge_info_ini = parse_ini_file($remote_merge_info);
-$merge = parse_ini_file("../../merge-info");
-if ($remote_release != $merge['release']){
-    echo('<input class="reg" type="button" value="Update from ' . $merge['release'] . ' to ' . $remote_release . '" onclick="location=\'update.php\'" />');
-}
-}
 ?>
 <head>
     <link href="/style.css" rel="stylesheet" type="text/css" />
