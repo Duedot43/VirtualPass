5423adfd24e77150eac12d3314f7f1979bc8acfc
Duedot43 <jackgendill@icloud.com>
2022-06-09 00:18:13 -0600

    Look Ma it works!

--- src/human_info/view.php
+++ src/human_info/view.php
@@ -5,9 +5,7 @@ if (!file_exists("../registered_phid/" . $_GET['user'])){
     echo "THAT USER DOES NOT EXIST!";
     exit();
 }
-$user_arr = json_decode(file_get_contents("../registered_phid/" . $_COOKIE['phid']), true);
 echo '<link href="/style.css" rel="stylesheet" type="text/css" />';
-unset($user_arr['activity']['cnum']);
 if (isset($_GET['date'])){
     $user_arr = json_decode(file_get_contents("../registered_phid/" . $_COOKIE['phid']), true);
     if (!isset($user_arr['activity'][$_GET['date']])){
@@ -16,14 +14,15 @@ if (isset($_GET['date'])){
     }
     unset($user_arr['activity'][$_GET['date']]['date']);
     foreach ($user_arr['activity'][$_GET['date']] as $date_arr){
-        echo "User USER left room " . file_get_contents("../registerd_qrids/" . $date_arr['room']) . " at TIME and arrived at TIME2 they were gone for MINUTES and " . $date_arr['timeDep'] - $date_arr['timeArv'] . " Seconds.";
+        echo "User " . $user_arr['fname'] . " " . $user_arr['lname'] . " left room " . file_get_contents("../registerd_qrids/" . $date_arr['room']) . " at " . date("H:i:s", $date_arr['timeDep']) . " and arrived at " . date("H:i:s", $date_arr['timeArv']) . " they were gone for " . ($date_arr['timeArv'] - $date_arr['timeDep'])/60 . " minutes and " . $date_arr['timeArv'] - $date_arr['timeDep'] . " Seconds.<br><br>";
 
     }
+    exit();
 }
-
+$user_arr = json_decode(file_get_contents("../registered_phid/" . $_COOKIE['phid']), true);
+unset($user_arr['activity']['cnum']);
 
 foreach ($user_arr['activity'] as $activ){
-    print_r($activ['date']);
     echo "<button class='reg' onclick='location=\"/human_info/view.php?user=" . $_GET['user'] . "&date=" . $activ['date'] . "\"' >" . $activ['date'] . "</button><br>";
 }
 ?>
