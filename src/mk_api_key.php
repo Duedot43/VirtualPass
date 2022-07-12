2c64959c49e5af62991a0d9f7f05452743b90b03
Duedot43 <gendillj@gmail.com>
2022-07-12 09:01:08 -0600

    mmmm i!

--- src/mk_api_key.php
+++ src/mk_api_key.php
@@ -4,6 +4,12 @@ include "api/modules.php";
 
 if (isset($_COOKIE['phid']) and is_numeric($_COOKIE['phid']) and file_exists("registered_phid/" . $_COOKIE['phid']) and file_exists("../mass.json")){
     $mass = readJson("../mass.json");
+    foreach ($mass['apiKeys'] as $user){
+        if ($_COOKIE['phid'] == $user){
+            echo "You akready have an API key it is " . key($user);
+            exit();
+        }
+    }
     $key = uniqid(rand()) . uniqid(rand());
     $mass['apiKeys'][$key] = $_COOKIE['phid'];
     file_put_contents("../mass.json", json_encode($mass));
