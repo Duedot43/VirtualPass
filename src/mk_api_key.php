332fabad66b64bf0e002443de7169897a62fdf46
Duedot43 <gendillj@gmail.com>
2022-07-12 08:53:52 -0600

    Angri

--- src/mk_api_key.php
+++ src/mk_api_key.php
@@ -3,7 +3,7 @@ include "api/modules.php";
 
 
 if (isset($_COOKIE['phid']) and is_numeric($_COOKIE['phid']) and file_exists("registered_phid/" . $_COOKIE['phid']) and file_exists("../mass.json")){
-    $mass = read_file("../mass.json");
+    $mass = readJson("../mass.json");
     $key = uniqid(rand()) . uniqid(rand());
     $mass['apiKeys'][$key] = $_COOKIE['phid'];
     file_put_contents("../mass.json", $mass);
