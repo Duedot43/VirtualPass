42353e2d49e450845b8a8cf1cf6db8f03a49b95e
Duedot43 <gendillj@gmail.com>
2022-07-13 17:07:02 -0600

    Fixed a bug in student.php

--- src/administrator/student.php
+++ src/administrator/student.php
@@ -52,10 +52,11 @@ function border($activity){
   }
 }
 $mass_json = json_decode(file_get_contents("../../mass.json"), true);
+echo '<link href="/style.css" rel="stylesheet" type="text/css" />';
 foreach ($mass_json['user'] as $user_id){
   $user_ini = parse_ini_file("../registered_phid/" . $user_id, true);
-  foreach ($user_ini['room'] as $user_room_id){
-    $tat = '<link href="/style.css" rel="stylesheet" type="text/css" /><input class="reg" type="button" value="' . $user_ini['usrinfo']['first_name'] . ' ' . $user_ini['usrinfo']['last_name'] . ' ' . $user_ini['usrinfo']['student_activity'] . '" onclick="location=\'/human_info/' . $user_id . '/index.html\'" style="border-color:' . border($user_ini['usrinfo']['student_activity']) . '; color:white"/></td><br>';
+  foreach ($user_ini as $user_room_id){
+    $tat = '<input class="reg" type="button" value="' . $user_ini['usrinfo']['first_name'] . ' ' . $user_ini['usrinfo']['last_name'] . ' ' . $user_ini['usrinfo']['student_activity'] . '" onclick="location=\'/human_info/' . $user_id . '/index.html\'" style="border-color:' . border($user_ini['usrinfo']['student_activity']) . '; color:white"/></td><br>';
     echo $tat;
   }
 }
