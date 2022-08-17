1085f1e07603332f146d0679a65a8dc632e928c4
Duedot43 <jackgendill@icloud.com>
2022-08-17 16:00:44 -0600

    e

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
-import multiprocessing, requests, json
+import multiprocessing, requests, json, random
+from more_itertools import first
 students = 3000
 studentsPerRoom = 20
-ClassesPerStudent = 8
-rooms = students/studentsPerRoom
+classesPerStudent = 8
+roomAmm = students/studentsPerRoom
+roomAmm = round(roomAmm)
+studentLst = {}
 vpHostname = "localhost:8080"
+names = requests.get("https://raw.githubusercontent.com/smashew/NameDatabases/master/NamesDatabases/first%20names/us.txt").text.replace("\r", "").split("\n")
+print(names)
 def createRooms():
     login = requests.post("http://" + vpHostname + "/teacher/cklogin.php", data={"uname": "teach", "passwd": "teach"}, allow_redirects=False)
     cookie = ""
@@ -14,8 +19,20 @@ def createRooms():
     adminKey = cookieLst[0]
     makeRoom = requests.post("http://" + vpHostname + "/teacher/mk_room/regqrid.php?room=")
 def makeStudents():
-    global students, rooms, ClassesPerStudent
-
+    global students, roomAmm, classesPerStudent, studentLst
+    firstRoom = 100
+    roomNumbers = []
+    for number in range(1,roomAmm):
+        roomNumbers.append(firstRoom)
+        firstRoom = firstRoom+1
+    for x in range(1,students):
+        studentID = random.randint(11111111111111111111, 99999999999999999999)
+        studentLst[str(studentID)] = {
+            "fname": "name",
+            "lname": "name",
+            "id": random.randint(111111111, 999999999),
+            "email": "email"
+        }
 def student(name):
     r = requests.post("http://" + vpHostname + "/index.php?room=")
-createRooms()
+makeStudents()
