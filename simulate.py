c5f15ef8423bb6f6d72ad954fe62fda555e1441a
Duedot43 <gendillj@gmail.com>
2022-08-18 19:03:48 -0600

    STOP IMPORTING TKINTER FOR NO REASON

--- simulate.py
+++ simulate.py
@@ -1,6 +1,4 @@
 import multiprocessing, requests, json, random, time
-from tracemalloc import start
-from turtle import st
 students = 3000 #This is basicly how many subprocesses you want lol
 studentsPerRoom = 30
 classesPerStudent = 8
