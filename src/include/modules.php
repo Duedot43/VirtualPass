274576b5bc0f81f7820218011ea9cb3d4e517efd
Duedot43 <jackgendill@icloud.com>
2022-08-22 14:26:05 -0600

    Main thing works

--- src/include/modules.php
+++ src/include/modules.php
@@ -1,21 +1,56 @@
 <?php
-function parseCsv(string $file) {
+function parseCsv(string $file)
+{
     $csvAsArray = array_map('str_getcsv', file("/home/syntax/Downloads/exportUsers_2022-8-19.csv"));
     return $csvAsArray;
 }
 function getDomain()
+{   
+    $host = explode(":", $_SERVER['HTTP_HOST'])[0];
+    return $host;
+}
+function sanatizeUser(array $userInfo)
 {
-    return $_SERVER['host'];
+    $userInfo[0] = preg_replace("/[^a-z.]+/i", "", $userInfo[0]);
+    $userInfo[1] = preg_replace("/[^a-z.]+/i", "", $userInfo[1]);
+    $userInfo[2] = preg_replace("/[^0-9.]+/i", "", $userInfo[2]);
+    $userInfo[3] = filter_var($userInfo[3], FILTER_VALIDATE_EMAIL);
+    return $userInfo;
 }
 
+function userExists($uname, $passwd, $db, $userKey) {
+    $output = mysqli_fetch_array(sendSqlCommand("SELECT * FROM users WHERE sysID=" . $userKey, $uname, $passwd, $db)[1]);
+    if (is_array($output)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function getUserData($uname, $passwd, $db, $key){
+    $response = sendSqlCommand("SELECT * FROM users WHERE sysID=" . $key, $uname, $passwd, $db)[1];
+    $data = mysqli_fetch_array($response);
+    return $data;
+}
+function roomExists($uname, $passwd, $db, $roomKey) {
+    $output = mysqli_fetch_array(sendSqlCommand("SELECT * FROM rooms WHERE ID=" . $roomKey, $uname, $passwd, $db)[1]);
+    if (is_array($output)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function getRoomData($uname, $passwd, $db, $key){
+    $response = sendSqlCommand("SELECT * FROM rooms WHERE ID=" . $key, $uname, $passwd, $db)[1];
+    $data = mysqli_fetch_array($response);
+    return $data;
+}
 function sendSqlCommand($command, $uname, $passwd, $db)
 {
     $srvName = "localhost";
-    try{
+    try {
         $conn = new mysqli($srvName, $uname, $passwd, $db);
         $result = $conn->query($command);
-    }
-    catch(Exception $error) {
+    } catch (Exception $error) {
         return [1, $error->getMessage()];
     }
     $conn->close();
@@ -25,28 +60,26 @@ function sendSqlCommandRaw($command, $uname, $passwd)
 {
     $srvName = "localhost";
 
-    try{
+    try {
         $conn = new mysqli($srvName, $uname, $passwd);
         $result = $conn->query($command);
-    }
-    catch(Exception $error) {
+    } catch (Exception $error) {
         return [1, $error->getMessage()];
     }
     $conn->close();
     return [0, $result];
 }
-class user
-{
-    public array $uInfo;
-    function read()
-    {
-        $userId = $this->uInfo['sysId'];
-    }
-    // Set the users info
-    public function install()
-    {
-    }
+function installUser($info, $uname, $passwd, $db){
+    $id = rand() . rand();
+    $out = sendSqlCommand("INSERT users VALUES('" . $id . "', '" . $info[0] . "', '" . $info[1] . "', '" . $info[2] . "', '" . $info[3] . "');", $uname, $passwd, $db);
+    $out[2] = $id;
+    return $out;
+
 }
-$csvAsArray = array_map('str_getcsv', file("/home/syntax/Downloads/exportUsers_2022-8-19.csv"));
-print_r($csvAsArray);
-?>
+function installRoom($info, $uname, $passwd, $db){
+    $id = $info['id'];
+    $out = sendSqlCommand("INSERT rooms VALUES('" . $id . "', '" . $info['num'] . "');", $uname, $passwd, $db);
+    $out[2] = $id;
+    return $out;
+
+}
--- src/index.php
+++ src/index.php
@@ -1,17 +1,52 @@
 <?php
 declare(strict_types=1);
 include "include/modules.php";
+$domain = getDomain();
 
 
-$config = parse_ini_file("../config.ini");
+$config = parse_ini_file("../config/config.ini");
+
+//create everything if it does not exist
 sendSqlCommandRaw("CREATE DATABASE IF NOT EXISTS VirtualPass;", "root", $config['sqlRootPasswd']);
+sendSqlCommand("CREATE TABLE IF NOT EXISTS users (
+    sysID varchar(255) NOT NULL,
+    firstName varchar(255),
+    lastName varchar(255),
+    ID varchar(255),
+    email varchar(255),
+    PRIMARY KEY (sysID)
+
+);", "root", $config['sqlRootPasswd'], "VirtualPass");
+sendSqlCommand("CREATE TABLE IF NOT EXISTS rooms (
+    ID varchar(255) NOT NULL,
+    num varchar(255),
+    PRIMARY KEY (ID)
+
+);", "root", $config['sqlRootPasswd'], "VirtualPass");
+
 // redirect to the main page
 if (!isset($_GET['room'])) {
     header('Location: /realIndex.php');
     exit();
 }
 
+if (!roomExists("root", $config['sqlRootPasswd'], "VirtualPass", preg_replace("/[^0-9.]+/i", "", $_GET['room']))) {
+    header('Location: /regRoom.php?room=' . $_GET['room']);
+    exit();
+}
+
 //if the user cookie is not set then redirect to register
 if (!isset($_COOKIE['id'])) {
-    header('Location: /regUser.php');
+    header('Location: /regUser.php?room=' . $_GET['room']);
+    exit();
+} elseif (!userExists("root", $config['sqlRootPasswd'], "VirtualPass", preg_replace("/[^0-9.]+/i", "", $_COOKIE['id']))) {
+    setcookie("id", "", time()-31557600, "/", $domain, true, true);
+    header('Location: /regUser.php?room=' . $_GET['room']);
+    exit();
+}
+
+
+
+if ( isset($_COOKIE['id']) and userExists("root", $config['sqlRootPasswd'], "VirtualPass", preg_replace("/[^0-9.]+/i", "", $_COOKIE['id']))) {
+
 }
