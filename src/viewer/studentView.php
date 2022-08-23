2d45461683ee92f96da4732cb9dd41535c803a1f
Duedot43 <jackgendill@icloud.com>
2022-08-23 08:21:01 -0600

    Hmmm

--- src/viewer/studentView.php
+++ src/viewer/studentView.php
@@ -9,6 +9,15 @@ if (!isset($_COOKIE['adminCookie']) or !adminCookieExists("root", $config['sqlRo
     header("Location: /admin");
     exit();
 }
+//View the user info
+if (isset($_GET['user'])) {
+    if (!userExists("root", $config['sqlRootPasswd'], "VirtualPass", preg_replace("/[^0-9.]+/i", "", $_GET['user']))) {
+        echo "That user does not exist.";
+        exit();
+    }
+    $user = getUserData("root", $config['sqlRootPasswd'], "VirtualPass", preg_replace("/[^0-9.]+/i", "", $_GET['user']));
+    
+}
 //cycle through all the users and display them
 $result = sendSqlCommand("SELECT * FROM users;", "root", $config['sqlRootPasswd'], "VirtualPass");
 while($row = mysqli_fetch_assoc($result[1])) {
