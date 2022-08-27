af466b7a12582cd374ca1bf24e0e90d7691d5b8e
Duedot43 <jackgendill@icloud.com>
2022-08-27 09:38:14 -0600

    That took way too long

--- src/include/mainScript.js
+++ src/include/mainScript.js
@@ -17,3 +17,24 @@ function dark_mode() {
         document.body.classList.toggle('')
     })
 }
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.responseText;
+    }
+    return result;
+}
+
+
+const vp_ver = loadFile("/public/version-info");
+parser = new DOMParser();
+xmlDoc = parser.parseFromString(vp_ver,"text/xml");
+console.log(vp_ver);
+
+
+// deepcode ignore DOMXSS: Stop it please
+document.getElementById('version-id').innerHTML = "Version ATS-" + xmlDoc.getElementsByTagName("version")[0].childNodes[0].nodeValue;
