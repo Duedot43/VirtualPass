fca3e8c3bbd869bf17ea6cf3de2b591a8c9e44a3
Duedot43 <gendillj@gmail.com>
2022-09-14 10:08:22 -0600

    More demo

--- demo.sql
+++ demo.sql
@@ -1,6 +1,6 @@
 USE VirtualPass;
-INSERT admins VALUES("bob", "joe", "123");
+INSERT admins VALUES("john", "doe", "123");
 INSERT apiKeys VALUES("123", "0", "123", "0");
 INSERT rooms VALUES("123", "123");
-INSERT teachers VALUES("bob", "joe", "123");
-INSERT users VALUES('123', 'bob', 'joe', '213', 'b@j.com', "1", '{"rooms": {}, "cnum": [0,0], "activity": {}}', '600');
+INSERT teachers VALUES("john", "doe", "123");
+INSERT users VALUES('123', 'john', 'doe', '213', 'j@d.com', "1", '{"rooms": {}, "cnum": [0,0], "activity": {}}', '600');
