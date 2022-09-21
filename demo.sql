9265d3b06cdc6c60528f1f95a64708b7fd065c4e
Duedot43 <gendillj@gmail.com>
2022-09-21 12:22:02 -0600

    How i did that dont ask me

--- demo.sql
+++ demo.sql
@@ -3,4 +3,4 @@ INSERT admins VALUES("john", "doe", "123");
 INSERT apiKeys VALUES("123", "0", "123", "0");
 INSERT rooms VALUES("123", "123");
 INSERT teachers VALUES("john", "doe", "123");
-INSERT users VALUES('123', 'john', 'doe', '213', 'j@d.com', "1", '{"rooms": {}, "cnum": [0,0], "activity": {}}', '600');
+INSERT users VALUES('123', 'john', 'doe', '213', 'j@d.com', "1", '{"rooms": {}, "cnum": [0,0], "dates": [], "activity": {}}', '600');
--- src/doActiv.php
+++ src/doActiv.php
@@ -31,54 +31,43 @@ if (isset($_COOKIE['id']) and userExists($config['sqlUname'], $config['sqlPasswd
     }
 
     $date = date("d") . "." . date("m") . "." . date("y");
-
+    $skip = false;
     if (!isset($departureData['activity'][$date])) {
-        $departureData['activity'][$date] = array();
-        $departureData['cnum'] = array(0, 0); 
         array_push($departureData['dates'], $date);
-        // TODO If the user decided to dtay departed into the next day then we need to look at the previous day (if there is one) and mark there arrived time we need to use the previous CNUM for this then we need to go as normal ::: i have 2 ways to do this i can do <this solution OR i could make it simpler by not recording the day only occorances then we can determin the day in the display by the timestamp and then sort but that might lead to more complexities and less viewable info in the DB
-        //TODO hmmm what does this do again?
-        /*
+        $departureData['activity'][$date] = array();
+        // Wow that is some DEEPLY nested stuff right there
+        if ((int) $userData['activ'] == 0 and count($departureData['dates']) > 1 and $departureData['activity'][$departureData['dates'][count($departureData['dates']) - 2]][$departureData['cnum'][0]]['timeArv'] == "") {
+            $departureData['activity'][$departureData['dates'][count($departureData['dates']) - 2]][$departureData['cnum'][0]]['timeArv'] = time();
+            $skip = true;
+        }
+        $departureData['cnum'] = array(0, 0);
+
+    }
+    if (!$skip) {
+        //determin the time
+        if ((int) $userData['activ'] == 1) {
+            $room = preg_replace("/[^0-9.]+/i", "", $_GET['room']);
+            $timeDep = time();
+            $timeArv = "";
+            $departureData['cnum'][1] = 1;
+            $set = false;
+        } else {
+            $room = $departureData['activity'][$date][$departureData['cnum'][0]]['room'];
+            $timeDep = $departureData['activity'][$date][$departureData['cnum'][0]]['timeDep'];
+            $timeArv = time();
+            $departureData['cnum'][1] = 0;
+            $set = true;
+        }
+
+        //mark down the time that the user does an activity
         $departureData['activity'][$date][$departureData['cnum'][0]] = array(
-            "room" => "",
-            "timeDep" => "",
-            "timeArv" => ""
+            "room" => $room,
+            "timeDep" => $timeDep,
+            "timeArv" => $timeArv
         );
-        */
-    }
-
-    //determin the time
-    if ((int) $userData['activ'] == 1) {
-        $room = preg_replace("/[^0-9.]+/i", "", $_GET['room']);
-        $timeDep = time();
-        $timeArv = "";
-        $departureData['cnum'][1] = 1;
-        $set = false;
-    } else {
-        //TODO bug over multipul days when the user is departed over days
-        $room = $departureData['activity'][$date][$departureData['cnum'][0]]['room'];
-        $timeDep = $departureData['activity'][$date][$departureData['cnum'][0]]['timeDep'];
-        $timeArv = time();
-        $departureData['cnum'][1] = 0;
-        $set = true;
-        //TODO HMMMMMM
-        /*
-        $departureData['activity'][$date][$departureData['cnum'][0] + 1] = array(
-            "room" => "",
-            "timeDep" => "",
-            "timeArv" => ""
-        );
-        */
-    }
-
-    //mark down the time that the user does an activity
-    $departureData['activity'][$date][$departureData['cnum'][0]] = array(
-        "room" => $room,
-        "timeDep" => $timeDep,
-        "timeArv" => $timeArv
-    );
-    if ($set) {
-        $departureData['cnum'][0] = $departureData['cnum'][0] + 1;
+        if ($set) {
+            $departureData['cnum'][0] = $departureData['cnum'][0] + 1;
+        }
     }
 
     //Depart or arrive the user
